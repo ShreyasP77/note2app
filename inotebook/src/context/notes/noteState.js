cf461c9f6e8c142d2474faeed577a7ed1fb1d25c
ShreyasP77 <80884270+ShreyasP77@users.noreply.github.com>
2021-12-29 00:18:21 +0530

    Update noteState.js

--- inotebook/src/context/notes/noteState.js
+++ inotebook/src/context/notes/noteState.js
@@ -4,7 +4,7 @@ import { useState } from 'react';
 
 const NoteState = (props) => {
 
-  const host = "https://note-2-app.herokuapp.com/"
+  const host = "https://note-2-app.herokuapp.com:5000/"
   const notesInitial = []
   const [notes, setNotes] = useState(notesInitial)
 
