8fe983c95c9ddb47e091844e71e379c68f20d159
ShreyasP77 <80884270+ShreyasP77@users.noreply.github.com>
2021-12-29 00:44:17 +0530

    Update Login.js

--- inotebook/src/components/Login.js
+++ inotebook/src/components/Login.js
@@ -12,7 +12,7 @@ const Login = (props) => {
   const handleSubmit = async (e) => {
 
     e.preventDefault()
-    const response = await fetch("https://note-2-app.herokuapp.com/api/auth/login", {
+    const response = await fetch("http://127.0.0.1:5000/api/auth/login", {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
