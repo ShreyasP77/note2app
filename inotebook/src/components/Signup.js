35bb0d16ee40eeecc19e0565838ff2f8d36707fb
ShreyasP77 <parkarshreyas77@gmail.com>
2021-12-28 20:46:28 +0530

    more changes

--- inotebook/src/components/Signup.js
+++ inotebook/src/components/Signup.js
@@ -12,7 +12,7 @@ const Signup = (props) => {
 
     const { name, email, password, cpassword } = credentials
     e.preventDefault()
-    const response = await fetch("http://localhost:5000/api/auth/", {
+    const response = await fetch("http://127.0.0.1:5000/api/auth/", {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
