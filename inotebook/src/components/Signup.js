6ca50f6a448d3443ff6886f6d1426c356558eb09
ShreyasP77 <80884270+ShreyasP77@users.noreply.github.com>
2021-12-29 14:42:55 +0530

    Update Signup.js

--- inotebook/src/components/Signup.js
+++ inotebook/src/components/Signup.js
@@ -12,11 +12,11 @@ const Signup = (props) => {
 
     const { name, email, password, cpassword } = credentials
     e.preventDefault()
-<<<<<<< HEAD
+
     const response = await fetch("https://note-2-app.herokuapp.com/api/auth/", {
-=======
+
     const response = await fetch("http://127.0.0.1:5000/api/auth/", {
->>>>>>> 7ba0ff32c1631b0804b617dc16331376d4371a81
+
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
